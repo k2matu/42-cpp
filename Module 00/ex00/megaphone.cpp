74edf9e3204c07b6dc0f7fbbbeb56261ff4614ba
k2matu <kstt90@gmail.com>
2024-08-15 00:02:40 +0300

    first 2 done but need to divide it up in smaller pieces

---
+++ Module 00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmatjuhi <kmatjuhi@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 21:16:57 by kmatjuhi          #+#    #+#             */
+/*   Updated: 2024/08/12 22:08:12 by kmatjuhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else
+	{
+		for (int i = 1; i < argc; i++)
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
